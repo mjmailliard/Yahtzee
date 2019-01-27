a09e6c78dc7815104ed158104fadbac4849f5dd2
Mike <mjmailliard@gmail.com>
2019-01-26 21:55:24 -0700

    initial commit

---
+++ src/Components/Scorecard.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Scorecard extends Component {
+  render(){
+    return(
+      <div>
+        Scorecard
+      </div>
+    )
+  }
+}
